3f18ad6cfa1f6ee6ff040102656fb264a193a0a1
cmz <mengzchen@qq.com>
2025-05-24 19:21:33 +0800

    c

--- ACM/7/C.cpp
+++ ACM/7/C.cpp
@@ -15,3 +15,28 @@
 保证3≤n≤10^5。
 使用C++编程，先分析题目，再编写程序。考虑边界情况与溢出
 */
+#include <iostream>
+
+using namespace std;
+
+unsigned long long calculateIntersections(int n) {
+    if (n < 4)
+        return 0;
+    unsigned long long res = 1;
+    // Calculate n * (n-1) / 2 * (n-2) / 3 * (n-3) / 4
+    res *= n;
+    res *= (n - 1);
+    res /= 2;
+    res *= (n - 2);
+    res /= 3;
+    res *= (n - 3);
+    res /= 4;
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << calculateIntersections(n) << endl;
+    return 0;
+}
